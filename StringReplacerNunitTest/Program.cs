385edc183e4686c50cb86ad71a75e3c84521296c
wanglin36 <wanglin36@CENTALINE.COM>
2016-11-02 17:33:55 +0800

    first commit

---
+++ StringReplacerNunitTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReplacerNunitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestStringReplacer.TestNoList();
+            TestStringReplacer.TestHasList();
+            TestStringReplacer.TestCompareWithListandNoList();
+            TestReplacer.Test();
+        }
+    }
+}
---
+++ StringReplacerNunitTest/TestReplacer.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReplacerNunitTest
+{
+    public class TestReplacer
+    {
+        public TestReplacer()
+        {
+        }
+        [Test]
+        public static void Test()
+        {
+
+            string source = "select * from class where #level# in (@param1,@param2,@param3) and #username# in (@param1,@param2,@param3)";
+          
+            string replaceResult = source.Replace("#level#", "#username#");
+           
+            bool boolResult;
+
+            boolResult = !string.IsNullOrEmpty(replaceResult);
+            Assert.IsTrue(boolResult, "Replace函数替换成功");
+            
+            //Assert.IsFalse(boolResult,"替换失败");
+
+
+        }
+    }
+}
